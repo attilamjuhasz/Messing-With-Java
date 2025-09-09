b3aad90f32110c6bf5567be7a368ae7e4c5f05a5
Attila Juhasz <106558542+attilamjuhasz@users.noreply.github.com>
2025-09-09 15:05:02 +0000

    Calculator

--- Fun.java
+++ Fun.java
@@ -1,1 +1,13 @@
+// Calculator
 
+import java.util.*;
+
+public class Fun{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        double x = scanner.nextDouble();
+        double y = scanner.nextDouble();
+        System.out.println(x / y);
+        scanner.close();
+    }
+}
