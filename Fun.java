f23d913b02c0af704de557af913637007c317723
Attila Juhasz <106558542+attilamjuhasz@users.noreply.github.com>
2025-10-01 14:49:18 +0000

    Tests

--- Fun.java
+++ Fun.java
@@ -1,13 +1,7 @@
-// Calculator
-
-import java.util.*;
-
 public class Fun{
     public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        double x = scanner.nextDouble();
-        double y = scanner.nextDouble();
-        System.out.println(x / y);
-        scanner.close();
+        String s = "String";
+        char c = 'c';
+        System.out.println(s + (char)(int)c);
     }
 }
