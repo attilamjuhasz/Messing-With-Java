95858ea666366a5bdac2f8e2e48f1e501582a5a8
Attila Juhasz <106558542+attilamjuhasz@users.noreply.github.com>
2026-03-05 18:25:37 -0500

    Added practice on sorts and finding algs

--- Fun.java
+++
@@ -1,7 +0,0 @@
-public class Fun{
-    public static void main(String[] args){
-        String s = "String";
-        char c = 'c';
-        System.out.println(s + (char)(int)c);
-    }
-}
---
+++ Test.java
@@ -0,0 +1,66 @@
+public class Test{
+
+    public static int bianary(int[] elements, int target){
+        int left = 0;
+        int right = elements.length - 1;
+        while (left <= right){
+            int middle = (left + right)/2;
+            if (target < elements[middle]){
+                right = middle-1;
+            }
+            else if (target > elements[middle]){
+                left = middle+1;
+            }
+            else {
+                return middle;
+            }
+        }
+        return -1;
+    }
+
+    public static void selectionSort(int[] elements){
+        for (int i = 0; i < elements.length - 1; i++){
+            int minIndex = i;
+            for (int k = i + 1; k < elements.length; k++){
+                if (elements[k] < elements[minIndex]){
+                    minIndex = k;
+                }
+            }
+            int temp = elements[i];
+            elements[i] = elements[minIndex];
+            elements[minIndex] = temp;
+        }
+    }
+
+    public static void insertionSort(int[] elements){
+        for (int i = 0; i < elements.length; i++){
+            int temp = elements[i];
+            int possibleIndex = i;
+            while (possibleIndex > 0 && temp < elements[possibleIndex - 1]){
+                elements[possibleIndex] = elements[possibleIndex -1];
+                possibleIndex--;
+            }
+            elements[possibleIndex] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 7, 1, 3};
+        int find = 3;
+        //System.out.println(Test.bianary(arr, find));
+
+        /*Test.selectionSort(arr);
+        for (int j = 0; j < arr.length; j++){
+            System.out.print(arr[j] + ", ");
+        }
+        System.out.println("");*/
+
+        int[] arrTwo = {4, 7, 12, 54, 1, -1, 3};
+        Test.insertionSort(arrTwo);
+        for (int k = 0; k < arrTwo.length; k++){
+            System.out.print(arrTwo[k] + ", ");
+        }
+        System.out.println("");
+    }
+    
+}
